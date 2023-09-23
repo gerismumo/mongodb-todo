da844ccf197feab199c7c2ea2f7fabc33789d8b2
gerismumo <geraldmumo6@gmail.com>
2023-09-23 10:03:12 +0300

    project arrangement

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
