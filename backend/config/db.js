c9bf8956e0eb713e1b8c8715adb52f9e84e3d8a1
gerismumo <geraldmumo6@gmail.com>
2023-09-23 16:04:07 +0300

    connection commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const MONGODB_URL = process.env.MONGODB_URL;
-        await mongoose.connect(MONGODB_URL);
+        await mongoose.connect(MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
         console.log('Connected successiful');
     } catch(error) {
         console.log('Error in connecting to the database',error);
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config({ path: 'config/.env' });
 const connectDB = require('./config/db')
 connectDB();
 app.use(cors());
